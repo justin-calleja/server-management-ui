43f0e0b8e94402480ea566f39d1e8cf2c8fae158
Justin Calleja <calleja.justin@gmail.com>
2018-09-30 11:59:08 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import serverSvg from './svg/server.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1 className="App-title">Server Canvas</h1>
+          <img src={serverSvg} className="App-logo" alt="logo" />
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
